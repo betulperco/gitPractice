3c9262bee66258b4c1f5a5cf7c73de819c840ebc
betulperco <140343688+betulperco@users.noreply.github.com>
2023-11-02 17:29:13 +0100

    Update Random.java
    Update changes in github

--- src/Random.java
+++ src/Random.java
@@ -4,5 +4,7 @@ public class Random {
         System.out.println("this class is about git practice");
 
         System.out.println("This is our new branch");
+
+        //this line is edited in github directly
     }
 }
