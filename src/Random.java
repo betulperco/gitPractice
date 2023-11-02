c1521426b182b15192c83ebf3094695c4d075c0e
Betul <betulperco@gmail.com>
2023-11-02 17:27:22 +0100

    project is created

--- src/Random.java
+++ src/Random.java
@@ -3,6 +3,6 @@ public class Random {
     public static void main(String[] args) {
 
         System.out.println("this class is about git practice");
-        System.out.println("thi is another change");
+        System.out.println("thi is another change about git practice");
     }
 }
